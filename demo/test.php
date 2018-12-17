d2d39cb8822fefb0dd6903959ca0dc5ef4a29cd6
SlashDim <dmitrikolesnikov1976@gmail.com>
2018-12-18 01:06:39 +0900

    two commit

--- demo/test.php
+++ demo/test.php
@@ -9,6 +9,7 @@
 <body>
 	<p>Примеры</p>
 	<hr>
+
 <p>1 пример</p>
 <?php
 	echo"Hello World!";
@@ -18,8 +19,29 @@
 <?
 	$name = "John"
 ?>
+<hr>
+
 <p>2 пример</p>
 <?=$name;?>
+<hr>
+
+<p>3 пример</p>
+<?
+define("USER_NAME", "Вася");
+echo USER_NAME;
+?>
+
+<hr>
+
+<p>3a пример</p>
+<?
+define("ONE_HUNDRED", "100");
+define("ONE_HUNDRED_TEN",ONE_HUNDRED + 10);
+echo ONE_HUNDRED_TEN
+?>
+<hr>
+
+<p>3b пример</p>
 </body>
 
 </html> 
