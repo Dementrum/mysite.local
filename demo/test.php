14984e34046f75a3f9a0f5a63a134fe35b88b064
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2018-12-17 17:59:36 +0900

    php date

---
+++ demo/test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+
+<body>
+	<p>Примеры</p>
+	<hr>
+<p>1 пример</p>
+<?php
+	echo"Hello World!";
+	echo strftime('%d-%B-%Y, %A');
+
+?>
+<?
+	$name = "John"
+?>
+<p>2 пример</p>
+<?=$name;?>
+</body>
+
+</html> 
