bda8a532bb4252aac0e3b73baa9815a65feea0ba
SlashDim <dmitrikolesnikov1976@gmail.com>
2018-12-27 00:23:04 +0900

    merge

--- table.php
+++ table.php
@@ -6,43 +6,43 @@ $rows = 10;
 <html>
 
 <head>
-  <title>Таблица умножения</title>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="style.css" />
+	<title>Таблица умножения</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
 
-  <div id="header">
-    <!-- Верхняя часть страницы -->
-    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-  </div>
+	<div id="header">
+		<!-- Верхняя часть страницы -->
+		<img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+		<span class="slogan">приходите к нам учиться</span>
+		<!-- Верхняя часть страницы -->
+	</div>
 
-  <div id="content">
-    <!-- Заголовок -->
-    <h1>Таблица умножения</h1>
-    <!-- Заголовок -->
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Количество колонок: </label>
-      <br />
-      <input name='cols' type='text' value="" />
-      <br />
-      <label>Количество строк: </label>
-      <br />
-      <input name='rows' type='text' value="" />
-      <br />
-      <label>Цвет: </label>
-      <br />
-      <input name='color' type='text' value="" />
-      <br />
-      <br />
-      <input type='submit' value='Создать' />
-    </form>
-    <!-- Таблица -->
-    <?php
+	<div id="content">
+		<!-- Заголовок -->
+		<h1>Таблица умножения</h1>
+		<!-- Заголовок -->
+		<!-- Область основного контента -->
+		<form action=''>
+			<label>Количество колонок: </label>
+			<br />
+			<input name='cols' type='text' value="" />
+			<br />
+			<label>Количество строк: </label>
+			<br />
+			<input name='rows' type='text' value="" />
+			<br />
+			<label>Цвет: </label>
+			<br />
+			<input name='color' type='text' value="" />
+			<br />
+			<br />
+			<input type='submit' value='Создать' />
+		</form>
+		<!-- Таблица -->
+		<?php
 			echo "<table border ='1';>";
 			for($tr=1; $tr<= $rows; $tr++) {
 				echo"<tr>";
@@ -56,31 +56,31 @@ $rows = 10;
 			}
 			echo"</table>";
 		?>
-    <!-- Таблица -->
-    <!-- Область основного контента -->
-  </div>
-  <div id="nav">
-    <h2>Навигация по сайту</h2>
-    <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a>
-      </li>
-      <li><a href='about.php'>О нас</a>
-      </li>
-      <li><a href='contact.php'>Контакты</a>
-      </li>
-      <li><a href='table.php'>Таблица умножения</a>
-      </li>
-      <li><a href='calc.php'>Калькулятор</a>
-      </li>
-    </ul>
-    <!-- Меню -->
-  </div>
-  <div id="footer">
-    <!-- Нижняя часть страницы -->
-    &copy; Супер Мега Веб-мастер, 2000 &ndash; 2015
-    <!-- Нижняя часть страницы -->
-  </div>
+		<!-- Таблица -->
+		<!-- Область основного контента -->
+	</div>
+	<div id="nav">
+		<h2>Навигация по сайту</h2>
+		<!-- Меню -->
+		<ul>
+			<li><a href='index.php'>Домой</a>
+			</li>
+			<li><a href='about.php'>О нас</a>
+			</li>
+			<li><a href='contact.php'>Контакты</a>
+			</li>
+			<li><a href='table.php'>Таблица умножения</a>
+			</li>
+			<li><a href='calc.php'>Калькулятор</a>
+			</li>
+		</ul>
+		<!-- Меню -->
+	</div>
+	<div id="footer">
+		<!-- Нижняя часть страницы -->
+		&copy; Супер Мега Веб-мастер, 2000 &ndash; 2015
+		<!-- Нижняя часть страницы -->
+	</div>
 </body>
 
 </html>
