674f2a2d611992e286ceb1836dace184ac68956f
Dementrum <dmi56756877@yandex.ru>
2019-10-28 02:15:04 +0900

    make calc

--- calc.php
+++ calc.php
@@ -1,17 +1,53 @@
-    <!-- Область основного контента -->
-    <form action=''>
+<?php
+function clearInt($data){
+    return (int)$data;
+}
+function clearStr($data){
+    return trim(strip_tags($data));
+}
+
+$output = '';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $num1 = clearInt($_POST['num1']);
+    $num2 = clearInt($_POST['num2']);
+    $operator = clearStr($_POST['operator']);
+    //Проверить поля
+    $output = "$num1 $operator $num2 = ";
+
+    switch ($operator) {
+        case '+': $output .= $num1 + $num2; break;
+        case '-': $output .= $num1 - $num2; break;
+        case '*': $output .= $num1 * $num2; break;
+        case '/':
+            if($num2 == 0)
+                $output='На ноль делить нельзя';
+            else
+                $output .= $num1 / $num2;
+            break;
+        default:
+            $output="Неизвестный оператор";
+    }
+}
+
+?>
+
+<!-- Область основного контента -->
+    <?php
+        if($output)
+            echo"<h3>Результат: $output </h3>";
+    ?>
+    <form action='<?= $_SERVER['REQUEST_URI'] ?>' method="post">
       <label>Число 1:</label>
       <br />
-      <input name='num1' type='text' />
+      <input name='num1' type='text' value="<?= $num1?>"/>
       <br />
       <label>Оператор: </label>
       <br />
-      <input name='operator' type='text' />
+      <input name='operator' type='text' value="<?= $operator?>"/>
       <br />
       <label>Число 2: </label>
       <br />
-      <input name='num2' type='text' />
-      <br />
+      <input name='num2' type='text' value="<?= $num2?>"/>
       <br />
       <input type='submit' value='Считать'>
     </form>
--- table.php
+++ table.php
@@ -1,27 +1,32 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$cols = abc((int) $_POST['cols']);
-	$rows = abc((int) $_POST['rows']);
+	$cols = abs((int)$_POST['cols']);
+	$rows = abs((int)$_POST['rows']);
+    $border = abs((int)$_POST['border']);
 	$color = trim(strip_tags($_POST['color']));
 }
 $cols = ($cols) ? $cols : 10;
-$rows = ($rows) ? $cols : 10;
-$border = ($border) ? $border : 3;
+$rows = ($rows) ? $rows : 10;
+$border = ($border) ? $border : 1;
 $color = ($color) ? $color : 'yellow';
 ?>
      <!-- Область основного контента -->
     <form action="<?= $_SERVER['REQUEST_URI'] ?>"method="POST">
         <label>Количество колонок: </label>
         <br />
-        <input name='cols' type='text' value="" />
+        <input name='cols' type='text' value="<?= $cols?>" />
         <br />
         <label>Количество строк: </label>
         <br />
-        <input name='rows' type='text' value="" />
+        <input name='rows' type='text' value="<?= $rows?>" />
+        <br />
+        <label>Рамка: </label>
+        <br/>
+        <input name='border' type='text' value="<?= $border?>" />
         <br />
         <label>Цвет: </label>
         <br />
-        <input name='color' type='text' value="" />
+        <input name='color' type='text' value="<?= $color?>" />
         <br />
         <br />
         <input type='submit' value='Создать' />
