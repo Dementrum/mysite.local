8fe3e6f832b5f42e2ee6dd0522918355dcb9b144
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2019-11-13 17:33:13 +0900

    file func

--- v2/demo/files/fopen.php
+++ v2/demo/files/fopen.php
@@ -1,9 +1,15 @@
-<HTML>
-<HEAD>
-<TITLE>fopen</TITLE>
-</HEAD>
-<BODY>
-<?
+
+<?php 
+$f = fopen("data.html", "r") or die("Не могу открыть файл!");
+/* fread($f, 5);
+fread($f, 4);
+fpassthru($f); */
+$lines = [];
+while ($line = fgetss($f, 4096, "<a>") ) {
+	$lines[] = $line;
+}
+print_r($lines);
+// Закрытие потока
+
+fclose($f);
 ?>
-</BODY>
-</HTML>
