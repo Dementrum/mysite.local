31b8da7a1f5616ce20dfb3f3a88954267a1d24c4
Dementrum <dmi56756877@yandex.ru>
2019-11-14 21:47:13 +0900

    fileread

--- v2/demo/files/fopen.php
+++ v2/demo/files/fopen.php
@@ -1,15 +1,20 @@
 
 <?php 
-$f = fopen("data.html", "r") or die("Не могу открыть файл!");
+$f = fopen("data.txt", "a+") or die("Не могу открыть файл!");
 /* fread($f, 5);
 fread($f, 4);
 fpassthru($f); */
-$lines = [];
-while ($line = fgetss($f, 4096, "<a>") ) {
-	$lines[] = $line;
-}
-print_r($lines);
+// $lines = [];
+// while ($line = fgetss($f, 4096, "<a>") ) {
+// 	$lines[] = $line;
+// }
+// print_r($lines) . "<br/>";
+// $bytes = [];
+// while (!feof($f) ) {
+// 	$bytes[] = fgetc($f);
+// }
+// print_r($bytes);
 // Закрытие потока
-
+fputs($f, "\nLine six");
 fclose($f);
 ?>
